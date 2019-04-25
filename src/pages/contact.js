1edefbb1ef880e031765e701e35789162c504c92
William McDermott <w.e.mcdermott@gmail.com>
2019-04-25 13:17:13 -0400

    add styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,7 @@ const ContactPage = () => {
   return (
     <Layout>
       <h1>Contact</h1>
-      <p>email: miketheb3ar@yahoo.com or go to <a href="https://www.google.com" target="_blank">Google</a></p>
+      <p>email: miketheb3ar@yahoo.com or go to <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">Google</a></p>
     </Layout>
   )
 }
