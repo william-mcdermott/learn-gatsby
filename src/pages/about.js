2b153f85f7b240764aae07b3059f30ceaa0c364b
William McDermott <w.e.mcdermott@gmail.com>
2019-04-25 10:45:59 -0400

    add about, blog, and contact pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>My name is William and I live in Austin, TX</p>
+    </div>
+  )
+}
+
+export default AboutPage
