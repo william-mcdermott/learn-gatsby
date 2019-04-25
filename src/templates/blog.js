5f2942c90ecbcbe5f7d9bc7b170f99348c66f09b
William McDermott <w.e.mcdermott@gmail.com>
2019-04-25 19:44:59 -0400

    add templates for blog posts

---
+++ src/templates/blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+const Blog = () => (
+    <Layout>
+      This is the blog template
+    </Layout>
+);
+
+export default Blog;
