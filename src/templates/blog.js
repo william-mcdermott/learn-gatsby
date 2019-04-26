7bca8d401da77402270e9896f38bd3f888c2c4f2
William McDermott <w.e.mcdermott@gmail.com>
2019-04-25 22:07:51 -0400

    add blog post body from contentful

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,43 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { graphql } from 'gatsby';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 import Layout from '../components/layout';
 
 export const query = graphql`
-  query (
-    $slug: String!
-  ) {
-    markdownRemark (
-      fields: {
-        slug: {
-          eq: $slug
-        }
+  query($slug: String!) {
+    contentfulBlogPost(slug: {eq: $slug}) {
+      title
+      publishedDate(formatString: "MMMM Do, YYYY")
+      body {
+        json
       }
-    ) {
-      frontmatter {
-        title
-        date
-      }
-      html
     }
-  }`;
+  }
+`;
 
-const Blog = props => (
+const Blog = (props) => {
+  const renderImage = (node) => {
+    const alt = node.data.target.fields.title['en-US'];
+    const { url } = node.data.target.fields.file['en-US'];
+    return <img alt={alt} src={url}/>;
+  };
+  const options = {
+    renderNode: {
+      'embedded-asset-block': renderImage
+    }
+  };
+  return (
     <Layout>
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
+      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <p>{props.data.contentfulBlogPost.publishedDate}</p>
+      {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
     </Layout>
-);
+  );
+};
 
 Blog.propTypes = {
   data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      html: PropTypes.node,
-      frontmatter: PropTypes.shape({
-        title: PropTypes.string,
-        date: PropTypes.string
+    contentfulBlogPost: PropTypes.shape({
+      title: PropTypes.string,
+      publishedDate: PropTypes.string,
+      body: PropTypes.shape({
+        json: PropTypes.json
       })
     })
   })
