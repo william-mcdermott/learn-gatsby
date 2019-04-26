acad3b5bdc36cd97543300b1e615cb0a20293468
William McDermott <w.e.mcdermott@gmail.com>
2019-04-25 22:47:51 -0400

    Clean up home page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,6 @@ const IndexPage = () => (
     <Head title="Home"/>
     <h1>Hello.</h1>
     <h2>I&#39;m William and I live in Austin</h2>
-    <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
   </Layout>
 );
 
