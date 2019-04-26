8aa97349c77f9066e74c3fb784b20a8923490013
William McDermott <w.e.mcdermott@gmail.com>
2019-04-25 20:13:17 -0400

    dynamically render blog post pages

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 
-const Blog = () => (
+export const query = graphql`
+  query (
+    $slug: String!
+  ) {
+    markdownRemark (
+      fields: {
+        slug: {
+          eq: $slug
+        }
+      }
+    ) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }`;
+
+const Blog = props => (
     <Layout>
-      This is the blog template
+      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+      <p>{props.data.markdownRemark.frontmatter.date}</p>
+      <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
     </Layout>
 );
+
+Blog.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      html: PropTypes.node,
+      frontmatter: PropTypes.shape({
+        title: PropTypes.string,
+        date: PropTypes.string
+      })
+    })
+  })
+};
 
 export default Blog;
