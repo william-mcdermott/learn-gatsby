6c3b5cd9246cb72c6b464d5f8ed56c5c6e5fcd18
William McDermott <w.e.mcdermott@gmail.com>
2019-04-25 21:19:28 -0400

    render list of blog posts from contentful rather than md files

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,16 +7,17 @@ import blogStyles from './blog.module.scss';
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost (
+        sort: {
+          fields: publishedDate,
+          order: DESC
+        }
+      ) {
         edges {
           node {
-            frontmatter {
-              title
-              date
-            }
-            fields {
-              slug
-            }
+            title
+            slug
+            publishedDate(formatString:"MMMM Do, YYYY")
           }
         }
       }
@@ -26,11 +27,11 @@ const BlogPage = () => {
     <Layout>
       <h1>Blog</h1>
       <ol className={blogStyles.posts}>
-        {data.allMarkdownRemark.edges.map(edge => (
-          <li key={edge.node.frontmatter.title} className={blogStyles.post}>
-            <Link to={`/blog/${edge.node.fields.slug}`}>
-              <h2>{edge.node.frontmatter.title}</h2>
-              <p>{edge.node.frontmatter.date}</p>
+        {data.allContentfulBlogPost.edges.map(edge => (
+          <li key={edge.node.title} className={blogStyles.post}>
+            <Link to={`/blog/${edge.node.slug}`}>
+              <h2>{edge.node.title}</h2>
+              <p>{edge.node.publishedDate}</p>
             </Link>
           </li>
         ))}
